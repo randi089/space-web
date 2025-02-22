111c670ce2e0b79e72380d1598c6bc7acd1ebcc8
Randi Febriadi <randifebriadi@gmail.com>
2025-02-22 22:01:13 +0700

    istirahat tidur

--- js/main.js
+++ js/main.js
@@ -59,7 +59,32 @@ const swiperTravel = new Swiper(".travel__swiper", {
 });
 
 // Show Scroll Up
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the
+  this.scrollY >= 350 ? scrollUp.classList.add("show-scroll") : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 // Scroll Sections Active Link
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(".nav__menu a[href*=]" + sectionId + "]");
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop * sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 // Scroll Reveal Animation
